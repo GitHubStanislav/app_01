eb22da5d320fe7ec9440e714e7ab60a19d03d0ec
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-04-13 19:53:46 +0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import PetInfo from "./components/PetInfo";
+import RandomNumber from "./components/RandomNamber";
 
 function App() {
   return (
     <div className="App">
       <PetInfo animal="cat" age="15" hasPet={true} />
       <PetInfo animal="dog" age={25} hasPet={false} />
+      <RandomNumber/>
     </div>
   );
 }
--- src/components/PetInfo.js
+++ src/components/PetInfo.js
@@ -1,10 +1,15 @@
+import "./PetInfo.css";
 function PetInfo(props) {
   const { animal, age, hasPet } = props;
   const text = hasPet
     ? `My ${animal} is 3 years ${age}`
     : "I don't have any pets";
 
-  return <h1>{text}</h1>;
+  return (
+    <div className="wrapper">
+      <h1>{text}</h1>
+    </div>
+  );
 }
 
 export default PetInfo;
