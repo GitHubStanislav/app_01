99877de1b1197e3ccf10125ad1840f8726f57e19
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-04-25 19:21:20 +0300

    +

--- src/components/jsonPosts/Posts.js
+++ src/components/jsonPosts/Posts.js
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
 import Post from "./Post";
 
+const API_URL = "https://jsonplaceholder.typicode.com/posts";
 function Posts() {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(true);
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    fetch(API_URL)
       .then((response) => response.json())
       .then((posts) => setPosts(posts))
-      .catch((error) => setError(error.message));
+      .catch((error) => setError(error.message))
+      .finally(() => setLoading(false));
   }, []);
 
   if (error) {
@@ -16,9 +20,13 @@ function Posts() {
   }
   return (
     <div>
-      {posts.map((post) => (
-        <Post key={post.id} {...post} />
-      ))}
+      <h1>Posts</h1>
+      <hr />
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        posts.map((post) => <Post key={post.id} {...post} />)
+      )}
     </div>
   );
 }
