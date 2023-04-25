656795571a613fa8f73f03c1ba8c36d8479327d2
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-04-25 17:52:08 +0300

    error.message

--- src/components/jsonPosts/Posts.js
+++ src/components/jsonPosts/Posts.js
@@ -3,12 +3,17 @@ import Post from "./Post";
 
 function Posts() {
   const [posts, setPosts] = useState([]);
+  const [error, setError] = useState("");
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((posts) => setPosts(posts))
-      .catch((error) => console.log(error.message));
+      .catch((error) => setError(error.message));
   }, []);
+
+  if (error) {
+    return <h1>Error:{error}</h1>;
+  }
   return (
     <div>
       {posts.map((post) => (
