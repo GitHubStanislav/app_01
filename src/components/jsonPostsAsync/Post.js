88ca85936c44c20aef3518fc9fecfe6375530c97
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-04-26 17:59:42 +0300

    +

---
+++ src/components/jsonPostsAsync/Post.js
@@ -0,0 +1,10 @@
+function Post({ userId, title, body }) {
+  return (
+    <div className="card">
+      <h1>{title}</h1>
+      <p>{body}</p>
+      <small>{userId}</small>
+    </div>
+  );
+}
+export default Post;
