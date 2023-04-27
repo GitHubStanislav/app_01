f3efbf3b508f455f3fa8fdb9ce4c367393b9df14
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-04-27 18:03:45 +0300

    +

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -1,7 +1,14 @@
 import Todo from "./Todo";
 
 function TodoList({ todos }) {
-  return todos.map((todo, index) => <Todo key={index} todo={todos} />);
+  return (
+    <div>
+      {!todos.length && <h2>Todo list is empty</h2>}
+      {todos.map((todo, index) => (
+        <Todo key={index} todo={todos} />
+      ))}
+    </div>
+  );
 }
 
 export default TodoList;
