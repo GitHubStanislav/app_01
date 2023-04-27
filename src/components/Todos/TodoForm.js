116930d724e2fa15578346c83a74e9b2782d9187
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-04-27 14:54:26 +0300

    +

--- src/components/Todos/TodoForm.js
+++ src/components/Todos/TodoForm.js
@@ -3,15 +3,19 @@ import { useState } from "react";
 function TodoForm() {
   const [toDo, setTodo] = useState("");
   const addTodo = (event) => {
-    event.preventDefault()
-    setTodo(event.target.value);
+    event.preventDefault();
   };
   return (
     <div>
       <form onSubmit={addTodo}>
         <label>
           <h1>Todo App</h1>
-          <input value={toDo} type="text" placeholder="Enter new todo " />
+          <input
+            onChange={(e) => setTodo(e.target.value)}
+            value={toDo}
+            type="text"
+            placeholder="Enter new todo "
+          />
         </label>
         <button>Submit</button>
       </form>
