e29b7a4bfb167694dd7d0180b5b1e62a83eadb4c
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-05-17 14:32:19 +0300

    refactor: add user error on screen

--- src/components/httpProjectJoke/App1.js
+++ src/components/httpProjectJoke/App1.js
@@ -6,6 +6,7 @@ import styles from "./App1.module.css";
 function App() {
   const [jokes, setJokes] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(null);
 
   const fetchJokesHandler = async () => {
     setIsLoading(true);
@@ -14,10 +15,11 @@ function App() {
         "https://official-joke-api.appspot.com/random_ten"
       );
       setJokes(response.data);
+      setIsLoading(false);
     } catch (error) {
+      setIsError(error);
       console.error(error);
     }
-    setIsLoading(false);
   };
 
   return (
@@ -27,8 +29,9 @@ function App() {
       </section>
       <section>
         {!isLoading && jokes.length > 0 && <JokeList jokes={jokes} />}
-        {!isLoading && jokes.length === 0 && <p>Jokes will be here:) )))</p>}
-        {isLoading && <p>Loading...</p>}
+        {!isLoading && jokes.length === 0 && <p>Jokes will be here:)))</p>}
+        {isError && <p>Error: {isError.message}</p>}
+        {isLoading && !isError && <p>Loading...</p>}
       </section>
     </>
   );
