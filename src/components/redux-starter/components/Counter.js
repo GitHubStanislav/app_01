da5b8e47b2640758ddc0b8b6380137b215813922
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-05-21 19:27:19 +0300

    +

--- src/components/redux-starter/components/Counter.js
+++ src/components/redux-starter/components/Counter.js
@@ -4,6 +4,7 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const dispatchFunction = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const isCounterIsVisible = useSelector((state) => state.isCounterIsVisible);
 
   const incrementHandler = () => {
     dispatchFunction({ type: "increment" });
@@ -11,14 +12,20 @@ const Counter = () => {
   const decrementHandler = () => {
     dispatchFunction({ type: "decrement" });
   };
-  const toggleCounterHandler = () => {};
+  const increaseHandler = () => {
+    dispatchFunction({ type: "increase", number: 10 });
+  };
+  const toggleCounterHandler = () => {
+    dispatchFunction({ type: "visibility" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Счётчик</h1>
-      <div className={classes.value}>{counter}</div>
+      {!isCounterIsVisible && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>+</button>
+        <button onClick={increaseHandler}>+10</button>
         <button onClick={decrementHandler}>-</button>
       </div>
       <button onClick={toggleCounterHandler}>Спрятать / Показать</button>
--- src/components/redux-starter/store/indexRedux.js
+++ src/components/redux-starter/store/indexRedux.js
@@ -1,16 +1,31 @@
 import { createStore } from "redux";
-
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, isCounterIsVisible: false };
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
       ...state,
       counter: state.counter + 1,
+      isCounterIsVisible: state.isCounterIsVisible,
+    };
+  }
+  if (action.type === "increase") {
+    return {
+      ...state,
+      counter: state.counter + action.number,
+      isCounterIsVisible: state.isCounterIsVisible,
     };
   }
   if (action.type === "decrement") {
     return {
       ...state,
       counter: state.counter - 1,
+      isCounterIsVisible: state.isCounterIsVisible,
+    };
+  }
+  if (action.type === "visibility") {
+    return {
+      counter: state.counter,
+      isCounterIsVisible: !state.isCounterIsVisible,
     };
   }
   return state;
